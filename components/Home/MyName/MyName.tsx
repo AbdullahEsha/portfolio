f8baf8c991de0def476af4c5defafb6d0bc75803
AbdullahEsha <shahariaresha@gmail.com>
2025-07-13 11:36:59 +0600

    update onlines

--- components/Home/MyName/MyName.tsx
+++ components/Home/MyName/MyName.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { motion } from '../../../node_modules/framer-motion/dist/framer-motion'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
+import React from "react";
+import { motion } from "../../../node_modules/framer-motion/dist/framer-motion";
+import Link from "next/link";
+import { useRouter } from "next/router";
 export default function MyName(props: { finishedLoading: boolean }) {
-  const router = useRouter()
+  const router = useRouter();
   return (
     <div
       className="h-full flex flex-col justify-center
@@ -79,23 +79,21 @@ export default function MyName(props: { finishedLoading: boolean }) {
         I&apos;m a <span className="text-AAsecondary">software engineer</span>,
         i possess strong problem-solving skills and specialize in crafting
         exceptional <br className="2xl:block hidden" />
-        digital experiences. My current area of focus is in the{' '}
+        digital experiences. My current area of focus is in the{" "}
         <span className="text-AAsecondary">
           Service Oriented Architecture like ERP and CRM
         </span>
-        , where I actively engage in developing{' '}
-        <span className="text-AAsecondary">microservices</span>,{' '}
-        <span className="text-AAsecondary">APIs</span>,{' '}
-        <span className="text-AAsecondary">databases</span>,{' '}
-        <span className="text-AAsecondary">back-end</span>,{' '}
-        <span className="text-AAsecondary">front-end</span> and{' '}
-        <span className="text-AAsecondary">full-stack</span> applications. I
-        have a strong understanding of the full software development life cycle,
-        from <br className="2xl:block hidden" />
-        creating and deploying them, as well as implementing the front-end
-        components to enable seamless user interactions.
-        {/* <br className="2xl:block hidden" />creating and deploying them, as well as implementing the
-        front-end components to enable seamless user interactions. */}
+        , where I actively engage in developing{" "}
+        <span className="text-AAsecondary">microservices</span>,{" "}
+        <span className="text-AAsecondary">APIs</span>,{" "}
+        <span className="text-AAsecondary">databases</span>,{" "}
+        <span className="text-AAsecondary">back-end</span>,{" "}
+        <span className="text-AAsecondary">front-end</span> and{" "}
+        <span className="text-AAsecondary">full-stack</span> applications.{" "}
+        <br className="2xl:block hidden" />I have a strong understanding of the
+        full software development life cycle, from creating and deploying them,
+        as well as implementing the front-end components to enable seamless user
+        interactions.
       </motion.h3>
       <motion.div
         initial={{ y: 10, opacity: 0 }}
@@ -112,12 +110,12 @@ export default function MyName(props: { finishedLoading: boolean }) {
         }}
         className="mt-12"
       >
-        <a href={'/resume.pdf'} target={'_blank'} rel="noreferrer">
+        <a href={"/resume.pdf"} target={"_blank"} rel="noreferrer">
           <button className="bg-AAprimary text-AAsecondary border rounded px-4 sm:px-8 py-3 sm:py-4 border-AAsecondary">
             Check out my resume!
           </button>
         </a>
       </motion.div>
     </div>
-  )
+  );
 }
